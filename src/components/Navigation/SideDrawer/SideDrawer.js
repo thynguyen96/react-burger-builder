f3c5a53c3d881cbea2a7bc1905b1ccd7b9758065
Phuong Tam Nguyen <phuong.tam_nguyen.ext@nokia.com>
2020-01-06 16:54:44 +0530

    Finished Lecture 181 Adding a Hamburger Icon

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from 'react'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import Logo from '../../Logo/Logo'
 import classes from './SideDrawer.module.css'
-
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
+    const attachedClasses = props.open ? [classes.SideDrawer, classes.Open].join(' ') : [classes.SideDrawer, classes.Close].join(' ')
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
